eb7fbbb3331f334647cc2e8f3e82f384de272cbb
Hygor Costa <cost0142@algonquinlive.com>
2022-09-19 11:35:12 -0400

    Re-Write Codding

--- src/index.js
+++ src/index.js
@@ -20,160 +20,38 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// get a reference to the database
 const db = getFirestore(app);
 
+// A variable that holds the id of the selected person.
+let selectedPersonId = null;
+
+// Arrays to hold data
+const people = [];
+const ideas = [];
+
 document.addEventListener("DOMContentLoaded", () => {
-  //set up the dom events
   document
     .getElementById("btnCancelPerson")
     .addEventListener("click", hideOverlay);
+
   document
     .getElementById("btnCancelIdea")
     .addEventListener("click", hideOverlay);
-  document.querySelector(".overlay").addEventListener("click", hideOverlay);
 
   document
     .getElementById("btnAddPerson")
     .addEventListener("click", showOverlay);
   document.getElementById("btnAddIdea").addEventListener("click", showOverlay);
-  getPeople();
-  getIdeas();
-});
 
-function hideOverlay(ev) {
-  ev.preventDefault();
-  document.querySelector(".overlay").classList.remove("active");
   document
-    .querySelectorAll(".overlay dialog")
-    .forEach((dialog) => dialog.classList.remove("active"));
-}
-function showOverlay(ev) {
-  ev.preventDefault();
-  document.querySelector(".overlay").classList.add("active");
-  const id = ev.target.id === "btnAddPerson" ? "dlgPerson" : "dlgIdea";
-  //TODO: check that person is selected before adding an idea
-  document.getElementById(id).classList.add("active");
-}
+    .getElementById("btnSavePerson")
+    .addEventListener("click", savePerson);
+  document.getElementById("btnSaveIdea").addEventListener("click", saveIdea);
 
-//  ------------------  Person ------------------
+  //  ----------------------
+  document
+    .querySelector(".person-list")
+    .addEventListener("click", handleSelectPerson);
 
-const peopleList = [];
-
-// Fetch Function Get People
-async function getPeople() {
-  const people = await getDocs(collection(db, "people"));
-  people.forEach((doc) => {
-    const data = doc.data();
-    const id = doc.id;
-    // console.log(data);
-    peopleList.push({ id, ...data });
-
-    return data;
-  });
-
-  buildPerson();
-}
-
-// Building People Function --> DOM
-function buildPerson() {
-  PageTransitionEvent;
-  let ul = document.querySelector(".person-list");
-
-  ul.innerHTML = "";
-
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  peopleList.forEach((person) => {
-    const dob = `${months[person["birth-month"] - 1]} ${person["birth-day"]}`;
-
-    let li = document.createElement("li");
-    li.setAttribute("id", `${person.id}`);
-    li.className = "person";
-
-    let personName = document.createElement("p");
-    personName.className = "person-name";
-    personName.innerHTML = `${person.name}`;
-
-    let personDob = document.createElement("p");
-    personDob.className = "person-dob";
-    personDob.innerHTML = `${dob}`;
-
-    li.append(personName, personDob);
-    ul.append(li);
-  });
-}
-
-const giftList = [];
-
-// Fetch Function Gift-ideas
-async function getIdeas() {
-  const ideas = await getDocs(collection(db, "gift-ideas"));
-  ideas.forEach((doc) => {
-    const data = doc.data();
-    const giftId = doc.id;
-    console.log(data);
-
-    giftList.push({ giftId, ...data });
-
-    return data;
-  });
-  buildingIdeas();
-
-  console.log(giftList);
-}
-
-// Building Ideas Function --> DOM
-function buildingIdeas() {
-  let ul = document.querySelector(".idea-list");
-
-  ul.innerHTML = "";
-
-  giftList.forEach((gift) => {
-    let li = document.createElement("li");
-    li.setAttribute("id", `${gift.id}`);
-    li.className = "idea";
-
-    let giftName = document.createElement("p");
-    giftName.className = "title";
-    giftName.innerHTML = `${gift.idea}`;
-
-    let giftLocation = document.createElement("p");
-    giftLocation.className = "location";
-    giftLocation.innerHTML = `${gift.location}`;
-
-    // EDIT
-    let editBtn = document.createElement("button");
-    editBtn.setAttribute("id", "edit-btn");
-    editBtn.className = "edit-btn";
-    editBtn.innerHTML = "Edit";
-
-    // DELETE
-    let deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("id", "delete-btn");
-    deleteBtn.innerHTML = "Delete";
-
-    // ADD label for
-    // ADD giftEdit for
-    // ADD giftDelete for
-
-    li.append(giftName, giftLocation, deleteBtn, editBtn);
-
-    ul.append(li);
-
-    console.log(gift);
-  });
-}
+  document.querySelector(".overlay").addEventListener("click", hideOverlay);
+});
